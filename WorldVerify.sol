160ac024b94f1e3cb4d55cde52d4d31f772b0936
paulman5 <mierlo.paul@hotmail.nl>
2024-08-05 23:18:41 +0200

    defined basic workings for Verificationlayer

--- WorldVerify.sol
+++ WorldVerify.sol
@@ -9,7 +9,20 @@ contract WorldVerify {
 
     mapping(address => uint256) AddressToWorldID;
     mapping(uint256 => address) WorldIDToAddress;
-        
     
+
+    ////////////////////////////////////////////////////////////////////////////////
+	///                                  ERRORS                                ///
+	//////////////////////////////////////////////////////////////////////////////
+
+    error WorldIDAlreadyUsed();
+
+    /// @dev The World ID instance that will be used for verifying proofs
+	IWorldID internal immutable worldId;
+
+    /// @dev Whether a nullifier hash has been used already. Used to guarantee an action is only performed once by a single person
+	mapping(uint256 => bool) internal nullifierHashes;  
+    
+
 
 }
