8dbb352c27a64d65d6c6ca3c98d139f3bdc8c158
paulman5 <mierlo.paul@hotmail.nl>
2024-08-05 20:33:39 +0200

    add boilerplate SC for DAO

---
+++ WorldVerify.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier
+
+import { IWorldID } from "./interfaces/IWorldID.sol";
+
+pragma solidity ^0.8.20;
+
+
+contract WorldVerify {
+
+    mapping(address => uint256) AddressToWorldID;
+    mapping(uint256 => address) WorldIDToAddress;
+
+    
+
+}
