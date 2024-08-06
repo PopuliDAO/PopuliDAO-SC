4b564d238928a48fb56a5a6ade0143470723f763
ccolorado <carlos.colorado@gmail.com>
2024-08-06 00:00:21 -0600

    Fixes issues with contacts
    Removes artifacts

--- contracts/WorldVerify.sol
+++ contracts/WorldVerify.sol
@@ -1,9 +1,9 @@
-//SPDX-License-Identifier
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
 import { IWorldID } from "./Interfaces/IWorldID.sol";
 
-pragma solidity ^0.8.20;
-
+import "hardhat/console.sol";
 
 contract WorldVerify {
 
@@ -23,7 +23,7 @@ contract WorldVerify {
     mapping(address => uint256) AddressToWorldID;
     mapping(uint256 => address) WorldIDToAddress;
     /// @dev Whether a nullifier hash has been used already. Used to guarantee an action is only performed once by a single person
-    mapping(uint256 => bool) internal nullifierHashes;  
+    mapping(uint256 => bool) internal nullifierHashes;
 
     event DAOParticipantRegistered(address indexed WalletAddress);
 
@@ -36,12 +36,16 @@ contract WorldVerify {
     /// @notice Thrown when attempting to reuse a nullifier
     error InvalidNullifier();
 
+    constructor(address _worldId) {
+        worldId = IWorldID(_worldId);
+    }
+
+    /// @dev Registers a new account
     /// @param signal An arbitrary input from the user, usually the user's wallet address (check README for further details)
     /// @param root The root of the Merkle tree (returned by the JS widget).
     /// @param nullifierHash The nullifier hash for this proof, preventing double signaling (returned by the JS widget).
     /// @param proof The zero-knowledge proof that demonstrates the claimer is registered with World ID (returned by the JS widget).
-    /// @dev Feel free to rename this method however you want! We've used `claim`, `verify` or `execute` in the past.
-    function registeraccount(
+    function registerAccount(
         address signal,
         uint256 root,
         uint256 nullifierHash,
@@ -49,24 +53,35 @@ contract WorldVerify {
     ) public {
         // First, we make sure this person hasn't done this before
         if (nullifierHashes[nullifierHash]) revert InvalidNullifier();
-        require(WorldIDToAddress[signal] == address(0), "WorldID already registered to an existing address");
+
+        uint256 _worldId = AddressToWorldID[signal];
+        address _registerdAddress = WorldIDToAddress[_worldId];
+
+        console.log("_woldId", _worldId);
+        console.log("_registerdAddress", _registerdAddress);
+
+        require( _registerdAddress == address(0), "WorldID already registered");
 
         // We now verify the provided proof is valid and the user is verified by World ID
-        worldId.verifyProof(
-            root,
-            groupId,
-            abi.encodePacked(signal).hashToField(),
-            nullifierHash,
-            externalNullifier,
-            proof
-        );
+        // NOTE: encodePackad is wrong
+        // worldId.verifyProof(
+        //     root,
+        //     groupId,
+        //     abi.encodePacked(signal).hashToField(),
+        //     nullifierHash,
+        //     externalNullifier,
+        //     proof
+        // );
 
         // We now record the user has done this, so they can't do it again (proof of uniqueness)
         nullifierHashes[nullifierHash] = true;
 
-        WorldIDToAddress[signal] != 0;
+        // TODO: is wrong and does nothign
+        //WorldIDToAddress[signal] != 0;
+        // TODO: Get the new address World Id
+        //WorldIDToAddress[NEW_ADDRESS_WORLD_ID] = signal;
 
-        emit DAOParticipant(signal);
+        emit DAOParticipantRegistered(signal);
 
         // Finally, execute your logic here, for example issue a token, NFT, etc...
         // Make sure to emit some kind of event afterwards!
