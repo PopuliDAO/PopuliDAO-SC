a9290259f0553bb6e2d15f11b5a702335dfdd839
Paul <mierlo.paul@hotmail.nl>
2024-08-07 21:40:23 +0200

    added createProposal and participant event

--- contracts/WorldVerify.sol
+++ contracts/WorldVerify.sol
@@ -7,6 +7,10 @@ import {IWorldID} from "./Interfaces/IWorldID.sol";
 import "hardhat/console.sol";
 
 contract WorldVerify {
+    struct DAOParticipant {
+        address walletaddress;
+    }
+
     using ByteHasher for bytes;
 
     /// @dev The contract's external nullifier hash
@@ -18,10 +22,6 @@ contract WorldVerify {
     /// @dev The World ID instance that will be used for verifying proofs
     IWorldID internal immutable worldId;
 
-    struct DAOParticipant {
-        address WalletAddress;
-    }
-
     // This can be a mapping solution for WorldID wallet address verification
     // mapping(address => uint256) AddressToWorldID;
     // mapping(uint256 => address) WorldIDToAddress;
@@ -32,7 +32,7 @@ contract WorldVerify {
     /// @dev Whether a nullifier hash has been used already. Used to guarantee an action is only performed once by a single person
     mapping(uint256 => bool) internal nullifierHashes;
 
-    event DAOParticipantRegistered(address indexed WalletAddress);
+    event DAOParticipantRegistered(address indexed walletaddress);
 
     ////////////////////////////////////////////////////////////////////////////////
     ///                                  ERRORS                                ///
@@ -45,7 +45,7 @@ contract WorldVerify {
 
     constructor(address _worldId, uint256 _externalNullifier) {
         worldId = IWorldID(_worldId);
-        externalNullifier = _externalNullifier;
+        externalNullifier = _externalNullifier; // refers to action + app_id, which is the same for each action and thus saves gas by inserting in the constructor
     }
 
     /// @dev Registers a new account
@@ -80,6 +80,13 @@ contract WorldVerify {
         // Finally, execute your logic here, for example issue a token, NFT, etc...
         // Make sure to emit some kind of event afterwards!
         WalletWhitelist[signal] = true;
+
+        emit DAOParticipantRegistered(signal);
         // Optional: emit DAOParticipantRegistered(signal);
+    }
+
+    function createProposal(address walletaddress) public {
+        require(WalletWhitelist[walletaddress], "Wallet is not whitelisted");
+        // Create a proposal
     }
 }
